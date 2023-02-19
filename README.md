21cd7b0307f5d9b83ba2e093ce8636a445a55667
reece-stephenson <reeces@bbd.co.za>
2023-02-19 21:49:09 +0200

    Constraints for soccerManagers & soccerCoaches

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Soccer-League-Database
 A Database project for the BBD Database Levelup
+
+## Authors
+
+Reece Stephenson
+[reece-stephenson](https://github.com/reece-stephenson)
+
+Palesa Mpe
+[Palesa-Mpe](https://github.com/Palesa-Mpe)
+
+Nicholas Joannou
+[NicholasJoannouBBD](https://github.com/NicholasJoannouBBD)
--- Soccer League DB.sql
+++ Soccer League DB.sql
@@ -82,30 +82,50 @@ CREATE TABLE [countries] (
 )
 GO
 
+-- Constraints for soccerTeams table
 ALTER TABLE [soccerTeams] ADD FOREIGN KEY ([stadiumID]) REFERENCES [stadiums] ([stadiumID])
+-- ALTER TABLE [soccerTeams] ADD CONSTRAINT pkSoccerTeamsTeamID PRIMARY KEY([teamID])
+ALTER TABLE [soccerTeams] ADD CONSTRAINT fkSoccerTeamsStadiumsStadiumID FOREIGN KEY([stadiumID]) REFERENCES [stadiums]([stadiumID])
+ALTER TABLE [soccerTeams] ADD CONSTRAINT unqSoccerTeamsTeamID UNIQUE([teamID])
+ALTER TABLE [soccerTeams] ADD CONSTRAINT chkSoccerTeamsTransferBudget CHECK([transferBudget] < 500000000)
 GO
 
+-- Constraints for persons table
 ALTER TABLE [persons] ADD FOREIGN KEY ([representingCountry]) REFERENCES [countries] ([countryID])
-GO
-
 ALTER TABLE [persons] ADD FOREIGN KEY ([teamID]) REFERENCES [soccerTeams] ([teamID])
+-- ALTER TABLE [persons] ADD CONSTRAINT pkPersonsPersonID PRIMARY KEY([personID])
+ALTER TABLE [persons] ADD CONSTRAINT fkPersonsSoccerTeamsTeamID FOREIGN KEY([teamID]) REFERENCES [soccerTeams]([teamId])
+ALTER TABLE [persons] ADD CONSTRAINT unqPersonsPersonID UNIQUE([personID])
+ALTER TABLE [persons] ADD CONSTRAINT chkPersonsDateOfBirth CHECK([dateOfBirth] < GetDate())
 GO
 
+-- Constraints for soccerPlayers table
 ALTER TABLE [soccerPlayers] ADD FOREIGN KEY ([personID]) REFERENCES [persons] ([personID])
+-- ALTER TABLE [soccerPlayers] ADD CONSTRAINT pkSoccerPlayersPersonID PRIMARY KEY([personID])
+ALTER TABLE [soccerPlayers] ADD CONSTRAINT unqSoccerPlayersPersonID UNIQUE([personID])
+-- ALTER TABLE [soccerPlayers] ADD CONSTRAINT chkSoccerPlayersHeight CHECK([positionType] = 'Goalkeeper' OR [height] > 190)
+-- ALTER TABLE [soccerPlayers] ADD CONSTRAINT chkSoccerPlayersNumber CHECK([positionType] <> 'Goalkeeper' AND [number] > 1)
+ALTER TABLE [soccerPlayers] ADD CONSTRAINT defSoccerPlayersPrefferedFoot DEFAULT 'Right' FOR [preferredFoot]
+-- Constraint for not having a soccer player with the same number in the same team?
 GO
 
+-- Constraints for soccerManagers table
 ALTER TABLE [soccerManagers] ADD FOREIGN KEY ([personID]) REFERENCES [persons] ([personID])
+ALTER TABLE [soccerManagers] ADD CONSTRAINT chkPositiveNumberTrophies CHECK ([trophiesWon] >= 0)
+ALTER TABLE [soccerManagers] ADD CONSTRAINT chkPositiveMatchesManaged CHECK ([matchesManaged] >= 0)
 GO
 
+-- Constraints for soccerCoaches table
 ALTER TABLE [soccerCoaches] ADD FOREIGN KEY ([personID]) REFERENCES [persons] ([personID])
+ALTER TABLE [soccerCoaches] ADD CONSTRAINT chkCoachTypes CHECK([coachType] in ('Goalkeeper', 'Defender', 'Midfielder', 'Attacker'))
+ALTER TABLE [soccerCoaches] ADD CONSTRAINT chkPositiveYearsExperience CHECK ([yearsExperience] >= 0)
 GO
 
+-- Constraints for soccerOwners table
 ALTER TABLE [soccerOwners] ADD FOREIGN KEY ([personID]) REFERENCES [persons] ([personID])
 GO
 
 ALTER TABLE [teamMatches] ADD FOREIGN KEY ([matchID]) REFERENCES [soccerMatches] ([matchID])
-GO
-
 ALTER TABLE [teamMatches] ADD FOREIGN KEY ([teamID]) REFERENCES [soccerTeams] ([teamID])
 GO
 
@@ -114,22 +134,3 @@ GO
 
 ALTER TABLE [stadiums] ADD FOREIGN KEY ([countryID]) REFERENCES [countries] ([countryID])
 GO
-
--- Constraints for soccerTeams table
--- ALTER TABLE [soccerTeams] ADD CONSTRAINT pkSoccerTeamsTeamID PRIMARY KEY([teamID])
-ALTER TABLE [soccerTeams] ADD CONSTRAINT fkSoccerTeamsStadiumsStadiumID FOREIGN KEY([stadiumID]) REFERENCES [stadiums]([stadiumID])
-ALTER TABLE [soccerTeams] ADD CONSTRAINT unqSoccerTeamsTeamID UNIQUE([teamID])
-ALTER TABLE [soccerTeams] ADD CONSTRAINT chkSoccerTeamsTransferBudget CHECK([transferBudget] < 500000000)
-
--- Constraints for persons table
--- ALTER TABLE [persons] ADD CONSTRAINT pkPersonsPersonID PRIMARY KEY([personID])
-ALTER TABLE [persons] ADD CONSTRAINT fkPersonsSoccerTeamsTeamID FOREIGN KEY([teamID]) REFERENCES [soccerTeams]([teamId])
-ALTER TABLE [persons] ADD CONSTRAINT unqPersonsPersonID UNIQUE([personID])
-ALTER TABLE [persons] ADD CONSTRAINT chkPersonsDateOfBirth CHECK([dateOfBirth] < GetDate())
-
--- Constraints for soccerPlayers table
--- ALTER TABLE [soccerPlayers] ADD CONSTRAINT pkSoccerPlayersPersonID PRIMARY KEY([personID])
-ALTER TABLE [soccerPlayers] ADD CONSTRAINT unqSoccerPlayersPersonID UNIQUE([personID])
--- ALTER TABLE [soccerPlayers] ADD CONSTRAINT chkSoccerPlayersHeight CHECK([positionType] = 'Goalkeeper' OR [height] > 190)
--- ALTER TABLE [soccerPlayers] ADD CONSTRAINT chkSoccerPlayersNumber CHECK([positionType] <> 'Goalkeeper' AND [number] > 1)
-ALTER TABLE [soccerPlayers] ADD CONSTRAINT defSoccerPlayersPrefferedFoot DEFAULT 'Right' FOR [preferredFoot]
